db7e5ffb739ed9c9bfff253e9678736852b32436
Nandun Thilina Bandara <ntbandara3@hotmail.com>
2018-10-14 22:04:47 +0530

    Implemented callback for GitHub authentication

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const axios = require('axios');
+const CONFIG = require('./config.json');
+
 const app = express();
 
 const PORT = 8090;
@@ -6,9 +9,31 @@ const PORT = 8090;
 app.use(express.static('public'));
 
 
+const CLIENT_ID         = CONFIG.CLIENT_ID;
+const CLIENT_SECRET     = CONFIG.CLIENT_SECRET;
+
+
 app.get('/', (req, res) => {
     res.sendFile('public/index.html');
 });
+
+// redirect endpoint
+app.get('/oauth/redirect', (req, res) => {
+
+    let requestToken = req.query.code;
+    
+    axios({
+        method: 'post',
+        url: `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}`,
+        headers: {
+            accept: 'application/json'
+        }
+    }).then((response) => {
+        const accessToken = response.data.access_token;
+        res.redirect(`/welcome.html?access_token=${accessToken}`)
+    });
+});
+
 
 app.listen(PORT, err => {
     if(err){
