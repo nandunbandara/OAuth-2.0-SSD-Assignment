c534a3a398e6a5c561197c41716b77857f51c787
Nandun Thilina Bandara <ntbandara3@hotmail.com>
2018-10-14 21:49:33 +0530

    public dir

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
 const app = express();
 
 const PORT = 8090;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile('public/index.html');
+});
 
 app.listen(PORT, err => {
     if(err){
